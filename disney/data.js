919c888b6f7513f9d54ec8b7bb92379c8d492b06
AlmazBakirov <93539854+AlmazBakirov@users.noreply.github.com>
2023-12-28 23:56:00 +0500

    28th

---
+++ disney/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: "falcon and the winter soldier",
+        des: "Those two heroes fight for the peach on Earth to make people colorful struggling against antiheroes",
+        image: "images/slider 2.png",
+    },
+    {
+        name: "loki",
+        des: "After escaping with sone Loki travel around the universe, but agency cought him to fix everything he has done",
+        image: "images/slider 1.png",
+    },
+    {
+        name: "wanda vision",
+        des: "Alaya witch goes crazy desiring to create her own controlled city, not taking responsibilities",
+        image: "images/slider 3.png",
+    },
+    {
+        name: "raya and the last dragon",
+        des: "Raya is small girl and happy who lives in island, she founds last dragon lets see what is going on",
+        image: "images/slider 4.png",
+    },
+    {
+        name: "Luca",
+        des: "Luca seems good person with good ideas to realize everything in real life",
+        image: "images/slider 5.png",
+    },
+]
